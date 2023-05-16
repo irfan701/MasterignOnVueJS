deac3d6bf7920079c408a489aca80b9491508451
irfan701 <irfanhossain701@gmail.com>
2023-05-17 01:39:48 +0400

    Vue CLI + MAKE First Component

---
+++ vue2/blog/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+</script>
+
+<template>
+    <HelloWorld msg="You did  this!" />
+  <br>
+    <Home/>
+</template>
+
+<style>
+  h1{
+    color: orange;
+  }
+</style>
+
+
+
+
---
+++ vue2/blog/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <h1 class="green">{{ msg }}</h1>
+</template>
+
+<style scoped>
+  h1{
+    color: orange;
+  }
+</style>
+
+
