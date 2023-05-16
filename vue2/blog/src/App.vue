b6373516ee28027b3623e70ef456ffdd317b195e
irfan701 <irfanhossain701@gmail.com>
2023-05-17 02:01:16 +0400

    interpolation+method with params

--- vue2/blog/src/App.vue
+++ vue2/blog/src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import Home from './components/Home.vue'
+import Interpolation from "./components/Interpolation.vue";
+
+
 </script>
 
 <template>
     <HelloWorld msg="You did  this!" />
   <br>
     <Home/>
+    <br>
+    <Interpolation/>
 </template>
 
 <style>
---
+++ vue2/blog/src/components/Interpolation.vue
@@ -0,0 +1,52 @@
+<template>
+
+
+    <h1>Name :{{name}}</h1>
+    <h2>Address:{{address}}</h2>
+    <h3>Age:{{age}}</h3>
+    <h3>CheckNumber:{{checkNumber==99}}</h3>
+    <h4>CallBack :{{getName()}}</h4>
+
+
+<span>{{setData("Azil Hossain")}}</span>
+
+    <p>{{getData()}}</p>
+
+</template>
+
+<script>
+    export default {
+        name: "Interpolation",
+        data() {
+            return {
+                name: "Irfan",
+                email:"irfanhossain701@gmail.com",
+                address: "Oli Mia Hat",
+                age: "30",
+                checkNumber:8899,
+                getName: function () {
+                    return "Callback Function"
+                }
+            }
+        },
+        methods:{
+            setData(name){
+                return name;
+            },
+            getData(){
+                return {
+                    Book:"Bangla English",
+                    Author:"JACKSON",
+                    Email:this.email
+
+                }
+            }
+
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>
